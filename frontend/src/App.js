247f0c3493fd91e209f2eca597cd5133a13223e8
Rounak2001 <rounakpatel2211@gmail.com>
2024-12-19 22:21:38 +0530

    frontend error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,4 @@
-// import React from 'react';
-// import ImageUpload from './components/ImageUpload';
-// import MapComponent from './components/MapComponent';
-// import FarmerDataTable from './components/TableData';
 
-// function App() {
-//   return (
-//     <div className="container mx-auto p-4">
-//       <div className="grid md:grid-cols-2 gap-4">
-//         <ImageUpload />
-//         <MapComponent />
-//         <FarmerDataTable />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 // App.js or your main container component
 import React from 'react';
